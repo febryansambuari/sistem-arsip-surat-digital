327ba1aacc503b6b5914b5c3e0189bb2edde61d0
Febryan Sambuari <febryan@definite.co.id>
2021-12-20 15:58:20 +0700

    change variable session on session file

--- config/session.php
+++ config/session.php
@@ -155,7 +155,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', 'https://sigit-staging.herokuapp.com/'),
+    'domain' => env('SESSION_DOMAIN', null),
 
     /*
     |--------------------------------------------------------------------------
